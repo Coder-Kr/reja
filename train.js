e357f6a523be581862534b9816c632dd3a2f09b1
Coder-Kr <jasurbekodilovn@mail.ru>
2024-06-02 00:18:05 +0900

    Feat: only F-task added

--- train.js
+++ train.js
@@ -1,11 +1,21 @@
-// E-TASK: 
+/*F-TASK: 
 
-// Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
-// MASALAN: getReverse("hello") return qilsin "olleh"
+Shunday findDoublers function tuzing, unga faqat bitta string argument pass bolib, agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
+MASALAN: getReverse("hello") return true return qiladi
+*/
 
 function getReverse(s){
-    return s.split('').reverse().join('');  
-}
+    let arr2 = [];
+    for (let v of s){
+      if(arr2.includes(v)){
+        return "true";
+      }
+      else
+        { 
+         arr2.push(v); 
+       }
+    }
+    return "false";
+  }
   
-console.log(getReverse("hello"));
-  
+  console.log(getReverse("hello"));
